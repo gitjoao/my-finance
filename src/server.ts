083161343b3c08daf45ecd32f99234f211178cc1
bitjoao <joao.moraes.batista@hotmail.com>
2026-05-06 21:21:51 -0300

    adicionando cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import "dotenv/config"
 import app from "./app"
 
-app.listen(3000, () => {
-  console.log("🚀 Server running on http://localhost:3000")
+app.listen(3333, () => {
+  console.log("🚀 Server running on http://localhost:3333")
 })
